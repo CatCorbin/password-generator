3a1d0ce8cd80a9a400943e1273726980af1cb95d
Cat Adams <catcorbin0919@gmail.com>
2023-07-10 14:58:31 -0600

    Created if statements

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var lower = "abcdefghijklmnopqrstuvwxyz".split("");
-var number = "0123456789".split("");
-var specialCharacter = "!@#$%^&*()_-+=/<>,.?:;~*".split("");
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var specialCharacter = "!@#$%^&*()_-+=/<>,.?:;~*";
 var password = [];
 var passwordCount = password.length;
 
@@ -19,27 +19,32 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var pass = [""];
+  //Questions asked to the user to tell us their password criteria
+  var pass = "";
   var noCharacters = prompt ("Choose password length between 8 and 128 characters.");
   var upperRequest = confirm ("Does your passsword require uppercase letters?");
   var lowerRequest = confirm ("Does your password require lowercase letters?");
   var numberRequest = confirm ("Does your password require numbers?");
   var specialRequest = confirm ("Does your password require any special characters?");
-
+  
   if (upperRequest === true) {
-    pass.concat(upper)
+    pass = pass.concat(upper)
   };
 
   if (lowerRequest === true) {
-    pass.concat(lower)
+    pass = pass.concat(lower)
   };
 
   if (numberRequest === true) {
-    pass.concat(number)
+    pass = pass.concat(number)
   };
 
   if (specialRequest === true) {
-    password = pass.concat(specialCharacter)
+    pass = pass.concat(specialCharacter)
+  };
+
+  for(var i=0;i<noCharacters.length;i++){
+    pass = Math.floor(Math.random() * noCharacters.length)
   };
 }
 
